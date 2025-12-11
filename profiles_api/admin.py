a5206b65fe7f643fbde222e9b5ac588465767a8c
ebraheem54 <ebraheem.sy370@gmail.com>
2025-12-12 02:23:11 +0300

    Added custom user profile model ,manager and migrations

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from profiles_api.models import UserProfile
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+admin.site.register(UserProfile)
